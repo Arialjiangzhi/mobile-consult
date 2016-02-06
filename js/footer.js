8deb8332f3b8cae5b9dadc825b8688866b486a3f
Arialjiangzhi <598729135@qq.com>
2016-02-06 13:06:30 +0800

    Signed-off-by: Arialjiangzhi <598729135@qq.com>

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+document.writeln("<div class=\"ask\">");
+document.writeln("    <p class=\"num_discrip\"><img class=\"icon_footer\" id=\"icon_footer_left\" src=\"images/icon_footer_ask.png\" width=\"20px\" height=\"18px\"><a href=\"#\">向TA提问</a></p>");
+document.writeln("  </div>");
+document.writeln("  <div class=\"add\">");
+document.writeln("    <p class=\"num_discrip\"><img class=\"icon_footer\" id=\"icon_footer_right\" src=\"images/icon_footer_add.png\" width=\"20px\" height=\"19px\"><a href=\"#\">关注TA</a></p>");
+document.writeln("  </div>");
